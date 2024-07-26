e6919dedcd229a8218fba8f237469e6d608aab8b
任思正 <2823259578@qq.com>
2024-07-26 13:16:00 +0800

    rsz

---
+++ src/ParentComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ChildComponent from "./ChildComponent";
+
+class ParentComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Parent Component</p>
+                <ChildComponent message={"Hello from parent!"} />
+            </div>
+        );
+    }
+}
+
+export default ParentComponent;
