7478306dc6e980dd93f8ce368a677bedaea0559b
任思正 <2823259578@qq.com>
2024-07-26 13:47:20 +0800

    11

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ParentComponent from "./ParentComponent";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <ParentComponent />
-            </div>
-        );
-    }
+import MyButton from "./MyButton";
+
+function App() {
+    return (
+        <div className="App">
+            <MyButton />
+        </div>
+    );
 }
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <ParentComponent />
+//           </div>
+          
+//         );
+//     }
+// }
 
 export default App;
---
+++ src/MyButton.js
@@ -0,0 +1,37 @@
+// import React, { useState } from "react";
+// import "./index";
+
+// function MyButton() {
+//     const [count, setCount] = useState(0);
+
+//     const handleClick = () => {
+//         setCount(count + 1);
+//     };
+
+//     return (
+//         <button className="my-button" onClick={handleClick}>
+//             {count}
+//         </button>
+//     );
+// }
+
+// export default MyButton;
+import React, { useEffect, useState } from "react";
+
+const MyButton = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    );
+};
+
+export default MyButton;
+
